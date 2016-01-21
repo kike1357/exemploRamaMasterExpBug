31939e3f117f16dabd8a6d8ed9e58205f9df7885
epastorizalorenzo <epastorizalorenzo@atanasoff21.danielcastelao.org>
2016-01-21 12:40:34 +0100

    commit gitHub2

---
+++ src/exemplomatriz/ExemploMatriz.java
@@ -0,0 +1,18 @@
+package exemplomatriz;
+
+/**
+ *
+ * @author epastorizalorenzo
+ */
+public class ExemploMatriz {
+
+    
+    public static void main(String[] args) {
+        MetodosMatriz obxMat= new MetodosMatriz();
+        obxMat.cargarMatriz();
+        obxMat.amosarMatriz();
+        obxMat.amosarMediaNotas();
+        obxMat.calcularMediaModulos();
+    }
+    
+}
